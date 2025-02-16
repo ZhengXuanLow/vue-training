5ae4881deb01157cb54ce68f95f1ea06799765c5
Zheng Xuan Low <lowxuan0218@gmail.com>
2025-02-16 23:49:36 +0800

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-    let message ="<u>Hello Vue</u>";
-    let name="content";
-    let checked = true;
+    // let message ="<u>Hello Vue</u>";
+    // let name="content";
+    // let checked = true;
+
+    let level=0;
+    let topics =["HTML","CSS","JavaScript","Vue,js"];
+    let topicsObject = {
+        HTML: "HyperText Markup Language",
+        CSS: "Cascading Style Sheets",
+        JavaScript: "JavaScript",
+        Vue: "Vue.js"
+    }
+
 </script>
 
 <template>
@@ -13,9 +23,31 @@
 
     <!-- <main v-html="message.toUpperCase()"> </main> -->
 
-    <main> {{message.toUpperCase()}} </main>
+    <!-- <main> {{message.toUpperCase()}} </main> -->
 
     <div :class="checked?'dark':'light'"> 操作標簽的屬性 </div>
+
+    <main> 
+        <div v-if="level===3">大神</div>
+        <div v-else-if="level===2">資深</div>
+        <div v-else-if="level===1">中階</div>
+        <div v-else>
+            <div>入門</div>
+            <ul >
+                <li v-for="name in topics" >{{name}}</li>
+            </ul>
+
+            <ul >
+                <li v-for="(name, index) in topics" >{{ index + 1}} - {{ name }}</li>
+            </ul>
+
+            <ul >
+                <li v-for="(value, key) in topicsObject" > {{key}}: {{value}}</li>
+            </ul>
+        </div>
+    </main>
+
+   
 
 </template>
 
